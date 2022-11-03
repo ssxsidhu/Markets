fb250c30f236d07e2ce549b4788b465c8a563f60
QZCarmen <91618371+QZCarmen@users.noreply.github.com>
2022-11-02 23:41:33 -0500

    database connection setup
    login, add events function, view applications created

--- js/index.js
+++ js/index.js
@@ -1,97 +1,92 @@
 
 $(document).ready(function () {
+    var faunadb = window.faunadb;
+    var q = faunadb.query;
+    var client = new faunadb.Client({
+        secret: 'fnAE0WnTguAATXwpxfX-ihF-aiYRMw8FJ9I_0nxl',
+        // Adjust the endpoint if you are using Region Groups
+        endpoint: 'https://db.us.fauna.com',
+    });
 
+    //get user info
+    var username = localStorage.getItem('username');
+    var user_events=[];
+    var flag = false;
+    if(username!=null){
+        $(".menu").html(generateMenu(username));//set up user menu
+        
+        client.query([
+            q.Map(
+                q.Paginate(q.Documents(q.Collection('User_Event'))),
+                q.Lambda(x => q.Get(x))
+            )
+        
+        ]).then(function (res) {
+            for(var i=0;i<res[0]["data"].length;i++){
+                var data =res[0]["data"][i]["data"];
+                if(data["username"] == username){
+                     user_events.push(data["event"]);
+                }
+             }
+             readEvents(client,q,user_events,username);
+        });
+    }else{
+        readEvents(client,q,[],username);
+    }
 
-    // readTextFile("db/test.json", function(text){
-    //     var data = JSON.parse(text);
-    //     console.log(data);
-    // });
-    //set up connection
-    // var faunadb = window.faunadb
-    // var q = faunadb.query
-    // var client = new faunadb.Client({
-    //     secret: 'fnAE0WnTguAATXwpxfX-ihF-aiYRMw8FJ9I_0nxl',
-    //     // Adjust the endpoint if you are using Region Groups
-    //     endpoint: 'https://db.us.fauna.com',
-    // })
-    // client.query([
-    //     q.Map(
-    //         q.Paginate(q.Documents(q.Collection('Events'))),
-    //         q.Lambda(x => q.Get(x))
-    //       )
-      
-    //   ])
-    // .then(function (res) { 
-    //     console.log('Result:', res);
-      
-    //     for(var i=0;i<res[0]["data"].length;i++){
-    //         var data =res[0]["data"][i]["data"];
-    //         console.log(data);
-    //          $(".events").append(generateView(data["date"],data["month"],data["pic"],data["name"],data["id"],data["description"]));
-    //     }
-    // })
-    // .catch(function (err) { console.log('Error:', err) })
+    //set up event list
+    
 
+    //on submit
+    $(document).on('click','.btn_terms',function(){
+        var id = $(this).attr("id");
+        id = parseInt(id.replace("btn_terms_",""));
+        var event = getEventById(id);
+        client.query(
+            q.Create(
+              q.Collection('User_Event'),
+              { data: { event: event,username:username } },
+            )
+          )
+          .then((ret) => console.log(ret));
+          //close current modal
+          $("#terms_"+id).modal("hide");
+          $("#successfulMsg").modal("show");
+
+    });
    
 
 });
 
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
+function readEvents(client,q,user_events,username){
+    client.query([
+        q.Map(
+            q.Paginate(q.Documents(q.Collection('Events'))),
+            q.Lambda(x => q.Get(x))
+          )
+      
+      ]).then(function (res) { 
+        console.log('Result:', res);
+      
+        for(var i=0;i<res[0]["data"].length;i++){
+            var data =res[0]["data"][i]["data"];
+            var found = false;
+             for(var j=0;j<user_events.length;j++){
+                if(data["id"]==user_events[j]["id"]){
+                    found=true;
+                }
+             }
+             $(".events").append(generateView(data["date"],data["month"],data["pic"],data["name"],data["id"],data["location"],data["description"],username,found));
+             if(!found){
+                 $(".terms").append(generateTermsModal(data["name"],data["id"],data["terms"],username));
+             }
         }
-    }
-    rawFile.send(null);
+    })
+    .catch(function (err) { console.log('Error:', err) });
 }
 
 
 
-function generateView(date, month,pic,name,id,description){
-    return`      <div class="card-container">
-    <div class="row border">
-        <div class="col-md-6  d-flex justify-content-start ">
-            <div class="photo-container">
-                <div class="date">
-                    <div class="day">`+date+`</div>
-                    <div class="month">`+month+`</div>
-                </div>
-            </div>
-            <img src="img/`+pic+`" class="rounded-0" alt="...">
-    
-        </div>
-  
-        <div class="col-md-6">
-            <div class="info-container d-flex justify-content-between">
-                <div class ="event-content">
-                    <div class="event-name">
-                        Event `+name+`
-                    </div>
-                    <div class="event-location">
-                        De-stress before finals with puppies!
-                    </div>
-                </div>
-                <a class="card-link float-right"> Apply as a vendor </a>
-               
-            </a>
-                <a class="card-link float-right" data-bs-toggle="collapse" href="#collapseExample`+id+`"
-                    aria-expanded="false" aria-controls="collapseExample`+id+`">
-                    <i class="fa fa-angle-down fa-2x" aria-hidden="true"></i>
-                </a>
-            </div>
-        </div>
 
-    </div>
-    <div class="row border">
-        <div class="collapse" id="collapseExample`+id+`">
-            <div class="card-body">
-                `+description+`
-            </div>
-        </div>
-    </div>
-</div>`;
 
-}
+
---
+++ js/login.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+$(document).ready(function () {
+ 
+
+    var all_users=[];
+    var faunadb = window.faunadb;
+    var q = faunadb.query;
+    var client = new faunadb.Client({
+        secret: 'fnAE0WnTguAATXwpxfX-ihF-aiYRMw8FJ9I_0nxl',
+        // Adjust the endpoint if you are using Region Groups
+        endpoint: 'https://db.us.fauna.com',
+    })
+    client.query([
+        q.Map(
+            q.Paginate(q.Documents(q.Collection('Users'))),
+            q.Lambda(x => q.Get(x))
+          )
+      ]).then(function (res) { 
+        console.log('Result:', res);
+       
+        for(var i=0;i<res[0]["data"].length;i++){
+           var data =res[0]["data"][i]["data"];
+           all_users.push(data);
+        }
+    })
+    .catch(function (err) { console.log('Error:', err) });
+
+    $("form").submit(function(e){
+        var match = false;
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var form = $("form");
+        if($(form)[0].checkValidity() === true){
+            for(var i =0;i<all_users.length;i++){
+                var user = all_users[i];
+                if(user["username"]==username){
+                    if(user["password"]==password){
+                        match=true;
+                        localStorage.setItem('username', username);
+                        window.location.href="index.html";
+                    }
+                }
+            }
+        }
+        return false;
+    });
+
+   
+});
+
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
