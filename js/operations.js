ccef8023dd94fe81817110aab4774890e16c34cf
ssxsidhu <sidhusahil111@gmail.com>
2022-11-17 01:50:08 -0600

    made a change when the navigation title appears on the main page

--- js/operations.js
+++ js/operations.js
@@ -3,14 +3,16 @@ $('body').on('click','.flex-row',function(){
         $('.flex-row.open').toggleClass('open');
     $(this).toggleClass('open');
     })
-    window.onload = function() {
-        if(window.location.href.includes('index.html')){            
-            $('.navigation-bar').append($('<span class="heading-text">').text("All events"))
-        }
-        else if(window.location.href.includes('myApplications.html')){
+
+window.onload = function() {
+        
+        if(window.location.href.includes('myApplications.html')){
             $('.navigation-bar').append($('<span class="heading-text" style="right:1%;">').text("My Applications"))
         }
         else if(window.location.href.includes('mySchedule.html')){
             $('.navigation-bar').append($('<span class="heading-text" style="right:4%;">').text("My Schedule"))
+        }
+        else{
+            $('.navigation-bar').append($('<span class="heading-text">').text("All events"))
         }
     }
