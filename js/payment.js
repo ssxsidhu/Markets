3ced84cc3f1c186edc746db70333763df559fada
minamary <minam@myumanitoba.ca>
2022-11-26 00:35:30 -0600

    Added additional payment validation

--- js/payment.js
+++ js/payment.js
@@ -12,4 +12,34 @@ $('#validationCustomUsername').on('keypress change', function() {
     });
   });
 
+  function isValidDate(){
 
+    var today, someday;
+    var exMonth=document.getElementById('selectedMonth');
+    console.log('MONTH:', exMonth.value);
+    var exYear=document.getElementById('selectedYear');
+    console.log('YEAR:', exYear.value);
+
+    today = new Date();
+    someday = new Date();
+    someday.setFullYear(exYear.value, exMonth.value - 1, someday.getDate());
+
+    if (someday < today) {
+      document.getElementById("errmsg").innerHTML = "Please select a valid expiry date";
+      return false;
+    } else {
+      document.getElementById("errmsg").innerHTML = "";
+      return true;
+    }
+
+    // document.getElementById("errmsg") = "You selected: " + x;
+    // today = new Date();
+    // someday = new Date();
+    // someday.setFullYear(exYear, exMonth, 1);
+    
+    // if (someday < today) {
+    //   alert("The expiry date is before today's date. Please select a valid expiry date");
+    //   return false;
+    // }
+  
+  }
